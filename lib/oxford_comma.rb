bf89796ccca0a344c280b129122b0042ff4edcce
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-22 21:14:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,9 @@ def oxford_comma(array)
   # s = array[0, array.size].join(', ') + ' and ' + array[array + 1]
   # puts s 
   # s 
+  if array.size == 1 
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  end 
 end
