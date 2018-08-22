8c79d458012a1e73e8f393044c58c7bd1c269eb4
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-22 21:13:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  s = array[0, array.size].join(', ') + ' and ' + array[array + 1]
-  puts s 
-  s 
+  # s = array[0, array.size].join(', ') + ' and ' + array[array + 1]
+  # puts s 
+  # s 
 end
